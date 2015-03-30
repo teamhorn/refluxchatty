53a34255173a9e9e985a95babb27cac638aeffb9
carlintj <carlin.therouxjones@gmail.com>
2015-03-30 13:15:39 +0000

    Updated comment counts now highlight

--- src/view/parentcomment.js
+++ src/view/parentcomment.js
@@ -16,6 +16,9 @@ var ParentComment = React.createClass({
       hidden: React.PropTypes.bool.isRequired,
       latestReply: React.PropTypes.string.isRequired
     },
+    getInitialState: function() {
+      return ({'highlightReplies': false});
+    },
     render: function() {
       if(this.props.hidden) return null;
       var props = this.props;
@@ -31,26 +34,27 @@ var ParentComment = React.createClass({
       
       if (props.replyCount > 0) {
         if(props.expanded) {
-          
           replyPosts = renderChildComments(props.threadId,props.children, 
               props.expandedChildId,props.replyingTo,props.username);
             
           replies = <div><span style={styles.clickable} 
             onClick={this.onCollapseClick}>Collapse</span></div>;
         } else {
-
+          var highlightClass = '';
+          if(this.state.highlightReplies) {
+            highlightClass = 'highlight';
+          }
           var replyStr = props.replyCount > 1 ? "replies" : "reply";
-          replies = <div><span style={styles.clickable} 
+          replies = <div ref="replies" className={highlightClass}><span style={styles.clickable} 
               onClick={this.onRepliesClick}>
               {props.replyCount} {replyStr}</span>
               &nbsp;<span style={styles.date}>Last reply @ : {props.latestReply}</span>
             </div>;
-        }
+          }
       } else {
         replies = <span>No replies</span>;
       }
-      
-      
+
       if(props.focused) {
         scroller = <AutoscrollingMixin parent={this} />
       } else {
@@ -78,6 +82,24 @@ var ParentComment = React.createClass({
           </div>
         </div>
       );
+    },
+    componentWillReceiveProps: function(nextProps) {
+      if(nextProps.replyCount != this.props.replyCount) {
+        this.setState({'highlightReplies': true});
+      } else {
+        this.setState({'highlightReplies': false});
+      }
+    },
+    componentDidUpdate: function() {
+      var repliesDiv = this.refs.replies;
+      if(repliesDiv) {
+        var el = React.findDOMNode(repliesDiv);
+        if(el.classList.contains('highlight')) {
+          setTimeout(function() {
+            el.classList.remove('highlight');
+          },10000);  
+        }
+      }
     },
     onRepliesClick: function() {
       ChattyActions.toggleParentComment(this.props.id);
