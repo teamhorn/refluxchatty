000d212a948702003a998773d6396d4a58b9a88a
carlintj <carlin.therouxjones@gmail.com>
2015-06-15 00:50:20 +0000

    Fixed ordering on posts

--- src/util/apiservice.js
+++ src/util/apiservice.js
@@ -59,6 +59,7 @@ var processThread = function(thread) {
       });
     }
     latestReply = Math.max(latestReply, fixedpost.date);
+    fixedpost.children = _.sortByOrder(fixedpost.children,'id', true);
     return fixedpost;
   };
 
